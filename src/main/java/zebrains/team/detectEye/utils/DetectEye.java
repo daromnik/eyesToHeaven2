bb2d658be8c18aafa1f1676fa334e2c10e23829d
r.danilov <r.danilov@zebrains.team>
2019-12-17 10:31:25 +0400

    Изменение классификатора для глаз

--- src/main/java/zebrains/team/detectEye/utils/DetectEye.java
+++ src/main/java/zebrains/team/detectEye/utils/DetectEye.java
@@ -17,6 +17,9 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.*;
+
+import static org.opencv.imgproc.Imgproc.circle;
 
 @Service
 @Log4j
@@ -28,7 +31,8 @@ public class DetectEye {
     private String imageFormat;
     private String imageName;
     private ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-    private final String CLASSIFIER_PATH = "haarcascades/haarcascade_eye.xml";
+    //private final String CLASSIFIER_PATH = "haarcascades/haarcascade_eye.xml";
+    private final String CLASSIFIER_PATH = "haarcascades/haarcascade_eye_tree_eyeglasses.xml";
 
     @Value("${spring.application.eyeFolder}")
     private String UPLOAD_FOLDER;
@@ -85,31 +89,57 @@ public class DetectEye {
      * @return boolean
      */
     private String detectAndSave(Mat originMat) throws IOException {
-        MatOfRect faces = new MatOfRect();
+        MatOfRect eyes = new MatOfRect();
         Mat grayFrame = new Mat();
 
         Imgproc.cvtColor(originMat, grayFrame, Imgproc.COLOR_BGR2GRAY);
         Imgproc.equalizeHist(grayFrame, grayFrame);
 
-        this.faceCascade.detectMultiScale(
-                grayFrame,
-                faces
-        );
+        this.faceCascade.detectMultiScale(grayFrame, eyes);
 
-        // каждый прямоугольник - это глаз
-        Rect[] facesArray = faces.toArray();
-        if (facesArray.length > 0) {
-            //берем первый элмент массива
-            Rect item = facesArray[0];
-            BufferedImage dest = originImage.getSubimage(item.x, item.y, item.width, item.height);
-            String eyeImageName = imageName + "_eye." + imageFormat;
-            String eyeImage = Paths.get(UPLOAD_FOLDER, eyeImageName).toString();
-            File fileForEye = new File(eyeImage);
-            ImageIO.write(dest, imageFormat, fileForEye);
-            return eyeImageName;
-        } else {
-            return "";
+        Rect[] eyesArray = eyes.toArray();
+        Arrays.sort(eyesArray, (r1, r2) -> (int)(r1.area() - r2.area()));
+        if (eyesArray.length > 0) {
+            for (Rect item : eyesArray) {
+                BufferedImage dest = originImage.getSubimage(item.x, item.y, item.width, item.height);
+                byte[] eyePixels = ((DataBufferByte) dest.getRaster().getDataBuffer()).getData();
+                Mat eyeMat = new Mat(dest.getHeight(), dest.getWidth(), CvType.CV_8UC3);
+                eyeMat.put(0, 0, eyePixels);
+
+                //if (isEye(eyeMat)) {
+                    String eyeImageName = imageName + "_eye." + imageFormat;
+                    String eyeImage = Paths.get(UPLOAD_FOLDER, eyeImageName).toString();
+                    File fileForEye = new File(eyeImage);
+                    ImageIO.write(dest, imageFormat, fileForEye);
+                    return eyeImageName;
+//                }
+//                else {
+//                    String eyeImageName = imageName + "_eye_error_x." + item.x + "_" + imageFormat;
+//                    String eyeImage = Paths.get(UPLOAD_FOLDER, eyeImageName).toString();
+//                    File fileForEye = new File(eyeImage);
+//                    ImageIO.write(dest, imageFormat, fileForEye);
+//                }
+            }
         }
+        return "";
+    }
+
+    public boolean isEye(Mat img) {
+        OpenCV.loadLocally();
+
+        Mat gray = new Mat();
+        Imgproc.cvtColor(img, gray, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.blur(gray, gray, new Size(3, 3));
+
+        Mat edges = new Mat();
+        int lowThreshold = 40;
+        int ratio = 3;
+        Imgproc.Canny(gray, edges, lowThreshold, lowThreshold * ratio);
+
+        Mat circles = new Mat();
+        Imgproc.HoughCircles(edges, circles, Imgproc.CV_HOUGH_GRADIENT, 1, 6, 100, 10, 3, 0 );
+
+        return !circles.empty();
     }
 
 }
